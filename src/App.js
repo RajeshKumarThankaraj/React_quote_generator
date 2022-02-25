1bd3309d14e7ccb0af88cc97cd3f1293422b7b79
Kumar Thankaraja <Rajesh.Thankaraja@finastra.com>
2022-02-25 18:39:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = { advice: "" };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+        this.setState({ advice: advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { advice } = this.state;
+    return (
+      <div className="app d-flex justify-content-center align-items-center">
+        <div className="container display-5">
+          <div className="card text-center">
+            <div className="card-header">Quote for the day!</div>
+            <div className="card-body">
+              <h5 className="card-title display-2">{advice}</h5>
+              <button
+                className="btn btn-outline-secondary"
+                onClick={this.fetchAdvice}
+              >
+                New Quote
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
